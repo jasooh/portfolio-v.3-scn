99e7fbbcc00fbabde57bfaa0c9a487408df683d1
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-06-06 18:13:47 -0400

    added images to timeline entries

--- components/TimelineEntry.tsx
+++ components/TimelineEntry.tsx
@@ -1,9 +1,42 @@
 // TimelineEntry.tsx
+// displays my jobs.
 
 import React from "react";
 
+// components
+import {AspectRatio} from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+
 // icons
 import {FaSuitcase} from "react-icons/fa";
+import {Button} from "@/components/ui/button";
+
+interface TimelineImagesProps {
+    images: string[];
+    className?: string;
+}
+
+function TimelineImages({images, className}: TimelineImagesProps) {
+    const limitedImages: string[] = images.slice(0, 3); // show up to 3 images
+
+    return (
+        <div className={className}>
+            {limitedImages.map((image, i) => (
+                <div key={i} className="w-40 relative">
+                    <AspectRatio ratio={16 / 9}>
+                        <Image
+                            src={image}
+                            alt={`Preview ${i + 1}`}
+                            className="rounded-md object-cover"
+                            fill
+                        />
+                    </AspectRatio>
+                </div>
+            ))}
+        </div>
+
+    )
+}
 
 interface TimelineEntryProps {
     title: string;
@@ -15,20 +48,23 @@ interface TimelineEntryProps {
 
 export default function TimelineEntry({title, description, currentJob = false, start, end}: TimelineEntryProps) {
     return (
-        <ol className={`pb-25 relative ${!currentJob && "border-s dark:border-gray-700"}`}>
+        <ol className={`pb-25 relative ${!currentJob && "border-s border-dashed dark:border-gray-700"}`}>
             <li className="ml-8 ms-6">
-                <span
-                    className="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 dark:ring-primary-dark dark:bg-primary">
-                    <span className={`absolute size-7 dark:bg-primary rounded-full z-[-1] ${currentJob && "animate-ping"}`} />
+                <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 dark:ring-primary-dark dark:bg-primary">
+                    <span className={`absolute size-7 dark:bg-primary rounded-full z-[-1] ${currentJob && "animate-ping"}`}/>
                     <FaSuitcase className="w-2.5 h-2.5" aria-hidden="true"/>
                 </span>
                 <h3 className="mb-1 text-lg font-semibold  dark:text-white">{title}</h3>
                 <time className="block mb-2 text-sm font-normal leading-none dark:text-gray-500">
                     {start} {end && `— ${end}`}
                 </time>
-                <p className="text-base font-normal dark:text-gray-400 mb-5">
+                <div className="text-base font-normal dark:text-gray-400 mb-5">
                     {description}
-                </p>
+                </div>
+                <TimelineImages
+                    className="my-4 flex flex-wrap gap-4"
+                    images={["https://placehold.co/768x512.png", "https://placehold.co/768x512.png"]}
+                />
             </li>
         </ol>
     );
