00284609768bb5d0e29a6c55fe3d4c536b05b74d
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-08-12 11:04:18 -0400

    better social icons

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,36 +1,37 @@
 // SocialLinks.tsx
 // renders all the socials on the home page.
 
-
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoDocumentTextSharp } from "react-icons/io5";
 import { SocialIcon } from "@/components/SocialIcon";
+import Image from "next/image";
+import React from "react";
 
 export default function SocialLinks() {
     return (
-        <nav aria-label="Social links" className="mt-2">
-            <ul className="flex flex-row items-center justify-center opacity-80 transition-opacity hover:opacity-100">
+        <nav aria-label="Social links">
+            <ul className="flex flex-row items-center gap-3">
                 <li>
-                    <SocialIcon
-                        href="https://github.com/jasooh"
-                        icon={FaGithub}
-                        label="GitHub"
-                    />
+                    <SocialIcon href="https://github.com/jasooh" label="GitHub">
+                        <FaGithub className="size-6 sm:size-7" />
+                    </SocialIcon>
                 </li>
                 <li>
-                    <SocialIcon
-                        href="https://www.linkedin.com/in/justin-abuyuan"
-                        icon={FaLinkedin}
-                        label="LinkedIn"
-                    />
+                    <SocialIcon href="https://www.linkedin.com/in/justin-abuyuan" label="LinkedIn">
+                        <FaLinkedin className="size-6 sm:size-7" />
+                    </SocialIcon>
                 </li>
                 <li>
-                    <SocialIcon
-                        href="/resume.pdf"
-                        icon={IoDocumentTextSharp}
-                        label="Resume"
-                        external={false}
-                    />
+                    {/* internal link; open in same tab */}
+                    <SocialIcon href="/resume.pdf" label="Resume" external={false}>
+                        <IoDocumentTextSharp className="size-6 sm:size-7" />
+                    </SocialIcon>
+                </li>
+                <li>
+                    <SocialIcon href="https://se-webring.xyz/" label="SE Webring">
+                        {/* decorative image: alt can be empty since link has aria-label */}
+                        <Image src="/logo_w.svg" alt="" width={28} height={28} className="h-7 w-7" />
+                    </SocialIcon>
                 </li>
             </ul>
         </nav>
--- components/sections/FooterSection.tsx
+++ components/sections/FooterSection.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoDocumentTextSharp } from "react-icons/io5";
 import { SocialIcon } from "@/components/SocialIcon";
+import SocialLinks from "@/components/SocialLinks";
 
 export default function FooterSection() {
     const year = new Date().getFullYear();
@@ -20,8 +21,8 @@ export default function FooterSection() {
                             <h3 className="relative font-bold text-2xl sm:text-3xl leading-[1.2] tracking-wide">
                                 {/* decorative glow */}
                                 <span aria-hidden="true" className="absolute inset-0 blur-xl">
-                  justin abuyuan
-                </span>
+                                    justin abuyuan
+                                </span>
                                 <span className="relative z-10 text-gray-200">justin abuyuan</span>
                             </h3>
                             <p className="italic text-gray-400">
@@ -30,32 +31,7 @@ export default function FooterSection() {
                         </div>
 
                         {/* social links */}
-                        <nav aria-label="Social links">
-                            <ul className="flex flex-row items-center gap-3">
-                                <li>
-                                    <SocialIcon href="https://github.com/jasooh" label="GitHub">
-                                        <FaGithub className="size-6 sm:size-7" />
-                                    </SocialIcon>
-                                </li>
-                                <li>
-                                    <SocialIcon href="https://www.linkedin.com/in/justin-abuyuan" label="LinkedIn">
-                                        <FaLinkedin className="size-6 sm:size-7" />
-                                    </SocialIcon>
-                                </li>
-                                <li>
-                                    {/* internal link; open in same tab */}
-                                    <SocialIcon href="/resume.pdf" label="Resume" external={false}>
-                                        <IoDocumentTextSharp className="size-6 sm:size-7" />
-                                    </SocialIcon>
-                                </li>
-                                <li>
-                                    <SocialIcon href="https://se-webring.xyz/" label="SE Webring">
-                                        {/* decorative image: alt can be empty since link has aria-label */}
-                                        <Image src="/logo_w.svg" alt="" width={28} height={28} className="h-7 w-7" />
-                                    </SocialIcon>
-                                </li>
-                            </ul>
-                        </nav>
+                        <SocialLinks />
                     </div>
 
                     {/* right: credits */}
