a912d9b80e449982cd9d2dee812b8291c31e835f
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-08-13 15:49:43 -0400

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,7 +31,8 @@ return (
             <CaptchaProvider>
                 {children}
             </CaptchaProvider>
-        <Analytics />
+            <SpeedInsights />
+            <Analytics />
         </body>
     </html>
 );
