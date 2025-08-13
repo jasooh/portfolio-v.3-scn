3c9879894a2cdaa42d6e67ec6d9208b4577ef6cb
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-08-13 16:35:17 -0400

    OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const baseUrl = new URL(process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.justin-abuyuan.xyz");
+
 export const metadata: Metadata = {
-  title: "Justin Abuyuan",
-  description: "Welcome to my website.",
+    metadataBase: baseUrl, // lets you use relative image URLs below
+    title: "Justin Abuyuan (v3)",
+    description:
+        "I'm Justin Abuyuan, an undergraduate Software Engineering student at the University of Waterloo. " +
+        "I'm interested in software development and machine intelligence.",
+    openGraph: {
+        type: "website",
+        url: baseUrl,
+        siteName: "Justin Abuyuan",
+        title: "Justin Abuyuan (v3)",
+        description:
+            "I'm Justin Abuyuan, an undergraduate Software Engineering student at the University of Waterloo. " +
+            "I'm interested in software development and machine intelligence.",
+        images: ["/opengraph-image"],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Justin Abuyuan (v3)",
+        description:
+            "I'm Justin Abuyuan, an undergraduate Software Engineering student at the University of Waterloo. " +
+            "I'm interested in software development and machine intelligence.",
+        images: ["/opengraph-image"],
+    },
 };
 
 export default function RootLayout({
