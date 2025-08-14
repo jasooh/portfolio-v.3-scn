43dccdba73e04b516dde8451c1056b7ee01b224d
Justin Abuyuan <abuyuanjustin@gmail.com>
2025-08-14 15:51:27 -0400

    msgs

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,9 +4,9 @@
 export const HERO_TEXT = [
     "i like to code stuff and build things for the internet.",
     "i turn ideas into pixels and apis.",
-    "i push to prod on purpose. (on my own apps 😁)",
     "i turn napkin sketches into production apps.",
     "i sweat details so everything just works.",
     "i measure, optimize, and deliver.",
-    "i learn fast and raise the bar."
+    "i learn fast and raise the bar.",
+    "i commit api keys to the repo. sk-r9t8L2qWvX4bY1zN7jK3pH5mE0aF6sD8oQ4uC1lG"
 ]
